1c0da8e17fe0f231f83c6f0cd62c433132b36509
Under <jorgeolmo.I@gmail.com>
2022-05-17 12:07:33 +0200

    Suma normal

--- challenges/20.js
+++ challenges/20.js
@@ -1,5 +1,43 @@
+/**
+ * 
+ * Reto #20: Parando el tiempo
+ * Dificutad: Media
+ * 
+ * Enunciado: Crea una funcion que sume dos numeros y retorne su resultado pasado unos segudos.
+ * - La funcion recibira por parametros los dos numeros a sumar y el tiempo en segundos que se demora en ejecutarse.
+ * - Si el lenguaje lo soporta, debera retornar el resultado de forma asincrona.
+ * 
+ */
+
+const prompt = require("prompt")
+prompt.start()
+
 function main () {
-    return 'test';
+    // Definimos los campos que necesitamos
+    var schema = {
+        properties: {
+            number1: {
+                description: 'Ingresa el primer numero: ',
+            },
+            number2: {
+                description: 'Ingresa el segundo numero: ',
+            },
+            time: {
+                description: 'Ingresa el tiempo en segundos: ',
+            }
+        }        
+    };
+    // Pedimos los datos al usuario
+    prompt.get(schema, (err, result) => {
+        if (err) { return onErr(err) }
+        console.log(`El resultado de la suma es: ${parseInt(result.number1) + parseInt(result.number2)}`);
+        
+    })
+}
+
+function onErr(err) {
+    console.log(err)
+    return 1
 }
 
 module.exports = {
